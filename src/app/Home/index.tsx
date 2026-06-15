83eb8c6a8bcc9d7aada738bbc561f9cd0cb849c0
Nicolas Capelari <nicolas.capelari@atos.net>
2026-06-15 11:44:52 -0300

    first commit

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,36 @@
+import { View, Image, TouchableOpacity, Text} from "react-native"
+
+import { FilterStatus } from "@/types/FilterStatus"
+
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+import { Filter } from "@/components/Filter"
+
+
+import { styles } from "./styles"
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <View style={styles.form}>
+        <Input placeholder="O que vc precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {
+            FILTER_STATUS.map((status) => (<Filter key={status} status={status} isActive />))
+          }
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
+}
